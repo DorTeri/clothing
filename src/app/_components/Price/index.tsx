b8c71f871d521ff87be9a9707ae068b7d3ddedfd
DorTeri <dortayari@gmail.com>
2024-05-01 18:00:34 +0300

    Fixed price

--- src/app/_components/Price/index.tsx
+++ src/app/_components/Price/index.tsx
@@ -6,9 +6,22 @@ import { Product } from '../../../payload/payload-types'
 
 import classes from './index.module.scss'
 
-export const priceFromJSON = (price: string, quantity: string = '1',raw?: boolean): string => {
-  const formattedPrice =  +price * +quantity
-  return formattedPrice.toString()
+export const priceFromJSON = (price: string, quantity: string = '1', raw?: boolean): string => {
+
+  const priceValue = parseFloat(price);
+    const quantityValue = parseFloat(quantity);
+
+    // Calculate the total price by multiplying price and quantity
+    const totalPrice = priceValue * quantityValue;
+
+    // Format the total price to include commas for thousands and a decimal point
+    const formattedPrice = totalPrice.toLocaleString('he-IL', {
+        style: 'currency',
+        currency: 'ILS'
+    });
+
+    // Return the formatted total price
+    return formattedPrice;
 }
 
 export const Price: React.FC<{
@@ -17,6 +30,8 @@ export const Price: React.FC<{
   button?: 'addToCart' | 'removeFromCart' | false
 }> = props => {
   const { product, product: { price } = {}, button = 'addToCart', quantity } = props
+  console.log("product", product)
+  console.log("price", price)
   const [formattedPrice, setFormattedPrice] = useState<string>('')
   const [priceWithQuantity, setPriceWithQuantity] = useState<string>('')
 
